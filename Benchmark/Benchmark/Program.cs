f880efd4451bf147bcccf91bb47bc145f1c775bc
Chris Z <chris@cipherspace.ch>
2019-03-22 21:41:08 +0100

    Reccursive

--- Benchmark/Benchmark/Program.cs
+++ Benchmark/Benchmark/Program.cs
@@ -7,15 +7,23 @@ namespace Benchmark
     {
         public const int NrOfNodes = 150;
         public const int NrOfLinks = 20;
+        public const int NrOfIterations = 10;
+    }
+
+    public static class Statistics
+    {
+        public static double NrOfCalcualtions = 0;
     }
 
     class Node
     {
         public Link[] Links;
+        public double Weight;
 
         public Node()
         {
             Links = new Link[Settings.NrOfLinks];
+            Weight = 0;
         }
     }
 
@@ -56,6 +64,32 @@ namespace Benchmark
             Trace.WriteLine(String.Format("Init in {0}ms", stopwatch.Elapsed.TotalMilliseconds));
 
             stopwatch.Restart();
+            nodes[0].Weight = 1;
+
+            calculateNode(0, nodes[0]);
+
+        }
+
+        public static void calculateNode(int iteration, Node node)
+        {
+            Trace.WriteLine(String.Format("{0} iteration:{1} Node:{2}", Statistics.NrOfCalcualtions, iteration, node));
+            Statistics.NrOfCalcualtions++;
+
+            if (iteration > Settings.NrOfIterations)
+            {
+                return;
+            }
+            else
+            {
+                for(int i = 0; i < Settings.NrOfLinks; i++)
+                {
+                    node.Links[i].Node.Weight += node.Weight * node.Links[i].Weight;
+                }
+                for (int i = 0; i < Settings.NrOfLinks; i++)
+                {
+                    calculateNode(iteration + 1, node.Links[i].Node);
+                }
+            }
         }
     }
 }
